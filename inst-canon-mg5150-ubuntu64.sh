df6635b3231660102d4c4fee9051bf5b6ea79f05
noodled <3495329+noodled@users.noreply.github.com>
2021-08-23 21:02:10 +0200

    Add files via upload

--- inst-canon-mg5150-ubuntu64.sh
+++ inst-canon-mg5150-ubuntu64.sh
@@ -1,7 +1,9 @@
-mkdir -p /download && cd /download
+mkdir -p /download 
+pushd /download
 wget https://github.com/noodled/libpng12-focal64/raw/main/libpng12-0_1.2.54-1ubuntu1.1%2B1~ppa0~focal_amd64.deb
 apt-get install -y libtiff5-dev
 dpkg -i libpng12-0_1.2.54-1ubuntu1.1+1~ppa0~focal_amd64.deb
+popd
 ###libtiff4_3.9.7-2ubuntu1_amd64.deb
 ###dpkg -i libpng12-0_1.2.54-1ubuntu1.1+1~ppa0~focal_amd64.deb
 ###dpkg -i cnijfilter-mg5100series_3.40-1_amd64.deb
